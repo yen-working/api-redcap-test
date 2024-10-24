e26f9e700c2266d7b7dc24e30eb21d5a77d72521
Yen <maiyn@ucmail.uc.edu>
2024-10-24 13:05:13 -0400

    Add DET class

--- api/main.py
+++ api/main.py
@@ -3,6 +3,19 @@ from pydantic import BaseModel
 from datetime import datetime
 class Item(BaseModel):
     name: str
+
+class RedCapDET(BaseModel):
+    project_id: int
+    username: str
+    instrument: str
+    record: int
+    redcap_event_name: str
+    redcap_data_access_group: str
+    # [instrument]_complete: int
+    redcap_repeat_instance: int
+    redcap_repeat_instrument: str
+    redcap_url: str
+    project_url: str
     
 app = FastAPI()
 items = []
@@ -22,5 +35,5 @@ def get_items():
     return items
 
 @app.post('/testPost')
-async def test():
-    return {"message": "POST request success"}
+async def test(det: RedCapDET):
+    return det
